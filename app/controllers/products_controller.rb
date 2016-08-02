04cc114ba964f7d46a2eaf24f035064c746c3f5e
lastromanticx <giladbarkan@live.com>
2016-08-02 13:40:00 -0400

    works in browser

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,27 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def create
+    product = Product.create(product_params)
+
+    redirect_to products_path
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: (if product.inventory > 0 then "true" else "false" end)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:price,:inventory,:description)
+  end
 end
